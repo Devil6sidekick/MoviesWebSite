aec13d97b4a162ac98aa942417c080cd7e6757ef
Devil6sidekick <redachahmi0@gmail.com>
2024-02-14 03:35:04 +0100

    Delete eclipse-workspace directory

--- eclipse-workspace/WSN/src/application/Sink.java
+++
@@ -1,9 +0,0 @@
-package application;
-
-public class Sink extends noeud {
-    public Sink(String modeleTransmissionReception, int x, int y) {
-        super(modeleTransmissionReception);
-        this.setX(x);
-        this.setY(y);
-    }
-}
